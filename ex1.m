f8f30e32c073521285e1952f28ce4e30571a141b
Simon Brenner <olsner@gmail.com>
2010-04-05 20:17:30 +0200

    Argument passing through varargs work, variable assignment doesn't.

--- ex1.m
+++ ex1.m
@@ -5,7 +5,7 @@ module Main;
 // TODO Maybe argc/argv should rather be queried from stdlib rather than
 int main(int argc, [const [const char]] argv)
 {
-	printf("Hello world\n");
-	test = 7;
+	printf("Hello world: %d\n", 7);
+	//test = 7;
 	return 123;
 }

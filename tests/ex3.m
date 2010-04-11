a4b3a92b7bb4edf20d6294b6dbe3b5d29129ad85
Simon Brenner <olsner@gmail.com>
2010-04-11 16:42:57 +0200

    Add another example to test some more things with string constants.

---
+++ tests/ex3.m
@@ -0,0 +1,15 @@
+import std::io;
+
+module Main;
+
+// TODO Maybe argc/argv should rather be queried from stdlib rather than
+int main(int argc, [const [const char]] argv)
+{
+	int test;
+	[const[const char]] tempString;
+	tempString = "Let's store a string in a variable";
+	test = 42;
+	printf("Hello world: %d (%s)\n", test, "And another string argument");
+	printf("%s\m", tempString);
+	return test;
+}

8ccf40eb5172e00f09b8187082011b6502945123
Simon Brenner <olsner@gmail.com>
2010-04-11 17:17:03 +0200

    Fix syntax (\m) and type error (that's not the type of string).

--- tests/ex3.m
+++ tests/ex3.m
@@ -1,16 +1,16 @@
 import std::io;
 import ex2;
 
-module Main;
+module ex3;
 
 // TODO Maybe argc/argv should rather be queried from stdlib rather than
 int main(int argc, [const [const char]] argv)
 {
 	int test;
-	[const[const char]] tempString;
+	[const char] tempString;
 	tempString = "Let's store a string in a variable";
 	test = 42;
 	printf("Hello world: %d (%s)\n", test, "And another string argument");
-	printf("%s\m", tempString);
+	printf("%s\n", tempString);
 	return test;
 }

410d7250838eee351bab1ec683e72b13a045c168
Simon Brenner <olsner@gmail.com>
2010-05-09 20:23:46 +0200

    Make the parser differentiate between retryable and final errors (the latter being caused by applications of 'commit').
    Also add support for better error messages.

--- Parser.hs
+++ Parser.hs
@@ -22,13 +22,14 @@ module Parser
   where
 
 import Control.Applicative
-import Control.Arrow
-
-import Control.Monad.State
+import Control.Arrow (first)
+import Control.Monad (unless)
 
 import Debug.Trace
 
-newtype Parser t a = P { unP :: [t] -> (Either String a, [t]) }
+data Res a = Success a | Retry String | Error String
+
+newtype Parser t a = P { unP :: [t] -> (Res a, [t]) }
 
 infixr 9 .:
 {-# INLINE (.:) #-}
@@ -37,11 +38,17 @@ infixr 9 .:
 {-# INLINE onP #-}
 onP f = P . f .: unP
 
-runParser = unP
+runParser :: Parser t a -> [t] -> (Either String a, [t])
+runParser (P p) = first convert . p
   where
-    convert r = case r of
-      Right a -> a
-      Left err -> error err
+    convert (Success x) = Right x
+    convert (Retry e) = Left e
+    convert (Error e) = Left e
+
+instance Functor Res where
+  fmap f (Success x) = Success (f x)
+  fmap _ (Retry e) = (Retry e)
+  fmap _ (Error e) = (Error e)
 
 instance Functor (Parser t) where
   {-# INLINE fmap #-}
@@ -49,20 +56,15 @@ instance Functor (Parser t) where
 
 instance Applicative (Parser t) where
   {-# INLINE pure #-}
-  pure x = P $ \ts -> (Right x, ts)
+  pure x = P $ \ts -> (Success x, ts)
   pf <*> (P pa) = continue `onP` pf
     where
-      continue (Left e, ts) = (Left e, ts)
-      continue (Right f, ts) = case pa ts of
-        (Right x, ts') -> (Right (f x), ts')
-        (Left e, ts') -> (Left e, ts')
-      -- first (Right . f) $ runParser pa ts
-
-commit (P p) = P $ \ts -> case p ts of
-  (Left e, ts) -> trace ("commit: "++e) (error e, ts)
-  (Right x, ts) -> (Right x, ts)
-
-failParse e = P $ \ts -> (Left e, ts)
+      continue (Error e, ts) = (Error e, ts)
+      continue (Retry e, ts) = (Retry e, ts)
+      continue (Success f, ts) = case pa ts of
+        (Success x, ts') -> (Success (f x), ts')
+        (Retry e, ts') -> (Retry e, ts')
+        (Error e, ts') -> (Error e, ts')
 
 {-instance Monad (Parser t) where
   return = pure
@@ -76,7 +78,13 @@ failParse e = P $ \ts -> (Left e, ts)
 instance Alternative (Parser t) where
   empty = failParse "Alternative.empty"
   {-# INLINE (<|>) #-}
-  (P p) <|> (P q) = P $ \ts -> case p ts of (Left _, _) -> q ts; r -> r
+  (P p) <|> (P q) = P $ \ts -> case p ts of (Retry _, _) -> q ts; r -> r
+
+commit (P p) = P $ \ts -> case p ts of
+  (Retry e, ts) -> trace "Retry caught in commit" (Error ("commit: "++e), ts)
+  other -> other
+
+failParse e = {-trace ("failParse: "++e) $-} P $ \ts -> (Retry e, ts)
 
 onFail = (<|>)
 
@@ -86,23 +94,24 @@ guardMsg b msg = unless b (failParse msg)
 next = P f
   where
     -- guardM (gets (not . null)) "Ran out of input (EOF)" >> (gets head <* modify tail)
-    f [] = (Left "Ran out of input (EOF)", [])
-    f (t:ts) = (Right t, ts)
+    f [] = (Retry "Ran out of input (EOF)", [])
+    f (t:ts) = (Success t, ts)
 
 lookNext = P f
   where
-    f [] = (Left "Ran out of input (EOF)", [])
-    f ts@(t:_) = (Right t, ts)
+    f [] = (Retry "Ran out of input (EOF)", [])
+    f ts@(t:_) = (Success t, ts)
 
 {-# INLINE satisfy #-}
-satisfyLook p = P f
+satisfyLook msg p = P f
   where
-    f [] = (Left "Ran out of input (EOF)", [])
-    f ts@(t:_) | p t = (Right t, ts)
-    f ts = (Left "Parser.satisfy: failed", ts)
-satisfy p = satisfyLook p <* next
+    f [] = (Retry ("Parser.satisfy: expected "++msg++" instead of EOF"), [])
+    f ts@(t:_)
+      | p t  = (Success t, ts)
+      | True = (Retry ("Parser.satisfy: expected "++msg++", found "++show t), ts)
+satisfy msg p = satisfyLook msg p <* next
 
-eof = P $ \ts -> (if null ts then Right () else Left "Expected end-of-file", ts)
+eof = P $ \ts -> (if null ts then Success () else Retry "Expected end-of-file", ts)
 
 list p ps = (:) <$> p <*> ps
 
@@ -115,7 +124,7 @@ exactly n p = list p (exactly (n - 1) p)
 sepBy1 p s = list p (many (s *> p))
 sepBy p s = sepBy1 p s <|> pure []
 
-match (x:xs) = list (satisfy (== x)) (match xs)
+match (x:xs) = list (satisfy (show x) (== x)) (match xs)
 match [] = return []
 
 {-# INLINE choice #-}

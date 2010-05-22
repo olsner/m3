32925d221d0ebf7d09562b6f5ce3e8fa91449750
Simon Brenner <olsner@gmail.com>
2010-05-22 22:56:51 +0200

    Add free and memset to stdlib.

--- stdlib/std/mem.m
+++ stdlib/std/mem.m
@@ -2,3 +2,5 @@ module std::mem;
 
 extern "C" [void] malloc(int size);
 extern "C" [void] realloc([void] ptr, int size);
+extern "C" void free([void]);
+extern "C" [void] memset([void] ptr, int c, int n);

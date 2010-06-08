7352b6ebc4b1e3fa6b91d16237e539a2ccf5b580
Simon Brenner <olsner@gmail.com>
2010-06-08 06:17:44 +0200

    Change conv1 test to not rely non-portably on perror.

--- tests/conv1.m
+++ tests/conv1.m
@@ -5,8 +5,8 @@ module conv1;
 int main(int argc, [const [const char]] argv)
 {
 	char[256] buf;
-	snprintf(buf, 256, "foo bar");
+	snprintf(buf, 256, "foo bar\n");
 	// Test that char-array can be converted to pointer-to-char and then to pointer-to-const-char
-	perror(buf);
+	printf(buf);
 	return 0;
 }

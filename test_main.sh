c191a696a88df8bf681695c6f7b8954b779ff6bc
Simon Brenner <olsner@gmail.com>
2010-06-08 06:24:30 +0200

    Extend test_main.sh with imports, convert conv1 to inline test.

--- test_main.sh
+++ test_main.sh
@@ -8,7 +8,12 @@ fail() {
 mod=temp_test
 rm tests/${mod}.m
 (
+while [ $# -gt 0 ]; do
+	echo "import $1;"
+	shift
+done
 cat <<EOF
+
 module ${mod};
 
 int main(int argc, [const [const char]] argv)
@@ -18,10 +23,9 @@ cat
 echo '}'
 ) > tests/${mod}.m
 
-dist/build/m3/m3 ${mod} >/dev/null 2>&1 || (
-	dist/build/m3/m3 ${mod}
+dist/build/m3/m3 ${mod} >/dev/null 2>&1 ||
+	dist/build/m3/m3 ${mod} ||
 	fail Compiling ${mod}
-) || exit $?
 file=${mod/::/__}
 llvm-as ${file}.ll || fail LLVM-assembling ${file}
 exec lli ${file}.bc "$@"

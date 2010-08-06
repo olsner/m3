bbb88b1512a892db21ce460902604b49ee072d98
Simon Brenner <olsner@gmail.com>
2010-08-06 03:35:39 +0200

    Inline all the combinators.

--- Parser/Internal.hs
+++ Parser/Internal.hs
@@ -39,16 +39,22 @@ newtype Parser s t a = P (forall r . s -> [t] -> Success s t a r -> Retry t r ->
 -- | Run a parser. Takes a state and a list of tokens and returns the remaining
 -- tokens along with either an error message or a tuple of state and value
 -- result.
+{-# INLINE runParser #-}
 runParser :: Parser s t a -> s -> [t] -> (Either String (a,s), [t])
 runParser (P p) s ts = p s ts suc err err
   where
+    {-# INLINE suc #-}
     suc = \a s ts -> (Right (a,s),ts)
+    {-# INLINE err #-}
     err = \ts msg -> (Left msg,ts)
 
 instance Functor (Parser s t) where
+  {-# INLINE fmap #-}
   fmap f (P pcf) = P $ \s ts suc -> pcf s ts (suc . f)
 instance Applicative (Parser s t) where
+  {-# INLINE pure #-}
   pure x = P $ \s ts suc _ _ -> suc x s ts
+  {-# INLINE (<*>) #-}
   P ff <*> P fx = P $ \s ts suc retr err ->
     let
       suc' = \f s ts -> fx s ts (suc'' f) retr err 
@@ -56,16 +62,20 @@ instance Applicative (Parser s t) where
     in ff s ts suc' retr err
 
 instance Alternative (Parser s t) where
+  {-# INLINE empty #-}
   -- Note: retry remembers its stream state - this allows a later soft-made-hard
   -- failure to provide the "rest of stream" to the parser user.
   empty = failParse "Alternative.empty"
   -- Run x with retry pointing to just running y from the current position.
   -- Error continuation remains the same (it will point outside of the parser)
+  {-# INLINE (<|>) #-}
   P x <|> P y = P $ \s ts suc retr err ->
     x s ts suc (\_ _ -> y s ts suc retr err) err
 
 instance Monad (Parser s t) where
+  {-# INLINE return #-}
   return = pure
+  {-# INLINE (>>=) #-}
   P x >>= y = P $ \s ts suc retr err ->
     let
       suc' = \x s ts -> let P f = y x in f s ts suc retr err
@@ -73,10 +83,12 @@ instance Monad (Parser s t) where
 
 -- | Make parse failure in 'p' a fatal parser error rather than a backtracking
 -- error.
+{-# INLINE commit #-}
 commit :: Parser s t a -> Parser s t a
 commit (P p) = P $ \s ts suc _ err -> p s ts suc err err
 -- i.e: simply replace the retry-continuation with the hard failure one.
 
+{-# INLINE failParse #-}
 -- | Cause a parse failure with the given message.
 failParse :: String -> Parser s t a
 failParse msg = P $ \_ ts _ retr _ -> retr ts msg
@@ -96,6 +108,7 @@ next = nextLook "next" tail
 look :: Parser s t t
 look = nextLook "look" id
 
+{-# INLINE eof #-}
 -- | Match only if at end of stream.
 eof :: Parser s t ()
 eof = P $ \s ts suc retr _ -> case ts of

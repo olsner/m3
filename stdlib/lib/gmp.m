c7784d6042a678c8b273a952e648b6ef3bbb1239
Simon Brenner <olsner@gmail.com>
2014-06-28 17:02:47 +0200

    Add pidigits implementation (doesn't compile yet though)

--- stdlib/lib/gmp.m
+++ stdlib/lib/gmp.m
@@ -1,7 +1,15 @@
 module lib::gmp;
 
 type mpz = struct { int alloc; int size; [void] limbs; };
+type mpz_t = [mpz];
 
-extern "C" void mpz_mul_2exp([mpz], [mpz], int);
 extern "C" void mpz_init([mpz]);
 extern "C" void mpz_init_set_ui([mpz], int);
+extern "C" void mpz_add([mpz], [mpz], [mpz]);
+extern "C" void mpz_mul_2exp([mpz], [mpz], int);
+extern "C" void mpz_mul_ui([mpz], [mpz], int);
+extern "C" void mpz_tdiv_q(mpz_t q, mpz_t n, mpz_t d);
+extern "C" /*unsigned long*/ int mpz_get_ui([mpz]);
+extern "C" void mpz_submul_ui(mpz_t rop, mpz_t op1, /*unsigned long*/ int op2);
+extern "C" void mpz_addmul_ui(mpz_t rop, mpz_t op1, /*unsigned long*/ int op2);
+extern "C" int mpz_cmp([mpz], [mpz]);
---
+++ tests/pidigits.m
@@ -0,0 +1,61 @@
+import lib::gmp;
+import std::lib;
+
+module pidigits;
+
+mpz[1] tmp1, tmp2, acc, den, num;
+type ui = /*unsigned*/ int;
+
+ui extract_digit(ui nth) {
+   // joggling between tmp1 and tmp2, so GMP won't have to use temp buffers
+   mpz_mul_ui(tmp1, num, nth);
+   mpz_add(tmp2, tmp1, acc);
+   mpz_tdiv_q(tmp1, tmp2, den);
+
+   return mpz_get_ui(tmp1);
+}
+
+void eliminate_digit(ui d) {
+   mpz_submul_ui(acc, den, d);
+   mpz_mul_ui(acc, acc, 10);
+   mpz_mul_ui(num, num, 10);
+}
+
+void next_term(ui k) {
+   ui k2 = k * 2 + 1;
+
+   mpz_addmul_ui(acc, num, 2);
+   mpz_mul_ui(acc, acc, k2);
+   mpz_mul_ui(den, den, k2);
+   mpz_mul_ui(num, num, k);
+}
+
+int main(int argc, [[const char]] argv) {
+   ui d, k, i;
+   int n = atoi(argv[1]);
+
+   mpz_init(tmp1);
+   mpz_init(tmp2);
+
+   mpz_init_set_ui(acc, 0);
+   mpz_init_set_ui(den, 1);
+   mpz_init_set_ui(num, 1);
+
+   i = k = 0;
+   while (i < n) {
+      next_term(++k);
+      if (mpz_cmp(num, acc) > 0)
+         continue;
+
+      d = extract_digit(3);
+      if (d != extract_digit(4))
+         continue;
+
+      putchar('0' + d);
+      if (++i % 10 == 0)
+         printf("\t:%u\n", i);
+      eliminate_digit(d);
+   }
+
+   return 0;
+}

6cbc2b1f7fb2ed00c76fc1a914f007ffa84c1af0
Simon Brenner <olsner@gmail.com>
2014-06-28 18:23:07 +0200

    Add putchar

--- stdlib/std/io.m
+++ stdlib/std/io.m
@@ -1,6 +1,8 @@
 
 module std::io;
 
+// Many of these actually return (e.g. errors)
 extern "C" void printf([const char] format, ...);
 extern "C" void snprintf([char] buf, int bufsize, [const char] format, ...);
 extern "C" void perror([const char] prefix);
+extern "C" void putchar(char c);
